003bacd515f3e8fbb6394e0537ab006b3ffd516f
Daniel Allen <dan@aeternagroup.net>
2026-07-29 23:36:53 +0100

    Update to schema version v5.0.0

--- variables.tf
+++ variables.tf
@@ -3,54 +3,20 @@ variable "private_dns_aaaa_records" {
 Map of private_dns_aaaa_records, attributes below
 Required:
     - name
+    - private_dns_zone_id
     - records
-    - resource_group_name
     - ttl
-    - zone_name
 Optional:
     - tags
 EOT
 
   type = map(object({
     name                = string
+    private_dns_zone_id = string
     records             = set(string)
-    resource_group_name = string
     ttl                 = number
-    zone_name           = string
     tags                = optional(map(string))
   }))
-  validation {
-    condition = alltrue([
-      for k, v in var.private_dns_aaaa_records : (
-        length(v.resource_group_name) <= 90
-      )
-    ])
-    error_message = "[from resourcegroups.ValidateName: invalid when len(value) > 90]"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.private_dns_aaaa_records : (
-        !endswith(v.resource_group_name, ".")
-      )
-    ])
-    error_message = "[from resourcegroups.ValidateName: must not end with \".\"]"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.private_dns_aaaa_records : (
-        length(v.resource_group_name) != 0
-      )
-    ])
-    error_message = "[from resourcegroups.ValidateName: invalid when len(value) == 0]"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.private_dns_aaaa_records : (
-        length(v.zone_name) > 0
-      )
-    ])
-    error_message = "must not be empty"
-  }
   validation {
     condition = alltrue([
       for k, v in var.private_dns_aaaa_records : (
@@ -59,6 +25,6 @@ EOT
     ])
     error_message = "[from tags.Validate: invalid when len(value) > 50]"
   }
-  # Note: 4 additional provider-side validators are enforced at apply time but not mirrored as validation{} blocks here (bespoke or non-mechanically-translatable).
+  # Note: 5 additional provider-side validators are enforced at apply time but not mirrored as validation{} blocks here (bespoke or non-mechanically-translatable).
 }
 
